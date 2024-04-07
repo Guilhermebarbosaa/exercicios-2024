18ad1faf7169317fb51eecd687e9d0e93a305897
Guilherme Dos Santos Barbosa <guilherme.barbosa221112@gmail.com>
2024-04-07 17:15:31 -0300

    após coleta de dados foi gerada a planilha a partir dos mesmos

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,28 +2,64 @@
 
 namespace Chuva\Php\WebScrapping;
  
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Common\Entity\Row;
+use PharIo\Manifest\Author;
 
 /**
  * Runner for the Webscrapping exercice.
  */
-
 class Main {
 
 
   public static function run(): void {
+
     $dom = new \DOMDocument('1.0', 'utf-8');
     @$dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-    $data = (new Scrapper())->scrap($dom);
-    print_r($data);
+    
+    $dadosPlanilha = (new Scrapper())->scrap($dom);
+
+    $caminhoPlanilha = 'C:\desenv\projeto\PLANILHA\dados_extraidos.xlsx';
+    $writer = WriterEntityFactory::createXLSXWriter();
+    $writer->openToFile($caminhoPlanilha);
+
+
+    $cabecalho = [
+      WriterEntityFactory::createCell('ID'),
+      WriterEntityFactory::createCell('Title'),
+      WriterEntityFactory::createCell('Type'),
+   ];
+
+   $qtdAuthors = 0;
+   foreach ($dadosPlanilha as $row) {
+      if  ($qtdAuthors<count($row->authors)){
+        $qtdAuthors = count($row->authors);
+      }
+   }
+   for ($i = 1; $i <= $qtdAuthors; $i++) {
+    $cabecalho[]= WriterEntityFactory::createCell ('Author '. $i);
+    $cabecalho[]=  WriterEntityFactory::createCell('Author '.$i. ' Institution');
+  }
+    $singleRow = WriterEntityFactory::createRow($cabecalho);
+    $writer->addRow($singleRow);
+
+    foreach ($dadosPlanilha as $paper) {
+    $cells = [
+      WriterEntityFactory::createCell($paper->id),
+      WriterEntityFactory::createCell($paper->title),
+      WriterEntityFactory::createCell($paper->type),
+    ]; 
+    foreach ($paper->authors as $var ){
+      $cells[] = writerEntityFactory::createCell($var->name);
+      $cells[] = writerEntityFactory::createCell($var->institution);
+
+    }
+    $singleRow = WriterEntityFactory::createRow($cells);
+    $writer->addRow($singleRow);
+  }
+   $writer->close();
+ 
   }
 
 }
-
-function createMain() {
-    return new Main();
-}
-
-$main = createMain();
-$main->run();
-
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -5,14 +5,7 @@ namespace Chuva\Php\WebScrapping;
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
 
-/**
- * Does the scrapping of a webpage.
- */
 class Scrapper {
-
-  /**
-   * Loads paper information from the HTML and returns the array with the data.
-   */
   public function scrap(\DOMDocument $dom): array {
    
     $papers = [];
