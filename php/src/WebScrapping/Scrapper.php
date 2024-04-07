ce26762b9f4d091ce3b12b07f88999c63c004b4f
Guilherme Dos Santos Barbosa <guilherme.barbosa221112@gmail.com>
2024-04-07 14:42:35 -0300

    extrair as informações do html e alimentou objeto da classe scrapper

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,17 +14,48 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
+   
+    $papers = [];
+
+    foreach($dom->getElementsByTagName('a') as $xpto) {
+
+      if ($xpto->getAttribute('class') == 'paper-card p-lg bd-gradient-left') {
+        $title = '';
+        $tipo = '';
+        $id = '';
+       
+        foreach($xpto->getElementsByTagName('div') as $div) {
+          if ($div->getAttribute('class') == 'volume-info') {
+            $id = $div->nodeValue;
+          }
+        }
+
+        foreach($xpto->getElementsByTagName('h4') as $h4) {
+          $title = $h4->nodeValue; 
+        }
+
+        foreach($xpto->getElementsByTagName('div') as $tagr) {
+          if ($tagr->getAttribute('class') == 'tags mr-sm') {
+            $tipo = $tagr->nodeValue;
+          }
+        }
+
+        $paper = new Paper($id, $title, $tipo);
+        
+        foreach ($xpto->getElementsByTagName('div') as $authors) {
+          if ($authors->getAttribute('class') == 'authors') {
+      
+            foreach ($authors->getElementsByTagName('span') as $span) {
+              $paper->authors[] = new Person(str_replace(';', '', $span->nodeValue), $span->getAttribute('title'));
+            }
+          }
+        }
+        
+        $papers[] = $paper;       
+      }    
+    } 
+
+    return $papers;
   }
 
 }
